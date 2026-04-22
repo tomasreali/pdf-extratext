974459b0c43bbcd09085f7e410499d791b8c13c5
tomasreali <tomi22as87@gmail.com>
2026-04-22 14:11:31 -0300

    feat(step-6): endpoints GET para listar todos los documentos y buscar por ID

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI, UploadFile, File, HTTPException
 from config.settings import settings
 from config.database import collection
+from bson.objectid import ObjectId
+from bson.errors import InvalidId
+from config.database import db
 import pdfplumber
 
 app = FastAPI(title=settings.app_name)
@@ -55,3 +58,41 @@ async def upload_pdf(file: UploadFile = File(...)):
         "filename": file.filename,
         "id": str(resultado.inserted_id)
     }
+
+@app.get("/documents")
+async def get_documents():
+    # 1. Buscamos en la base de datos (ocultando el texto gigante)
+    cursor = collection.find({}, {"texto": 0})
+    
+    documentos = []
+    # 2. Recorremos uno por uno
+    for doc in cursor:
+        # 3. Convertimos el ObjectId a texto normal
+        doc["_id"] = str(doc["_id"]) 
+        documentos.append(doc)
+        
+    return documentos
+
+# --- NUEVO: Subtareas 2 y 3 (Buscar un documento específico) ---
+@app.get("/documents/{doc_id}")
+def get_document_by_id(doc_id: str):
+    documento = None
+    
+    # Intento 1: Lo buscamos como ObjectId (El formato nativo de Mongo)
+    try:
+        obj_id = ObjectId(doc_id)
+        documento = db["documents"].find_one({"_id": obj_id})
+    except InvalidId:
+        pass # Si el formato no sirve para ObjectId, ignoramos y pasamos al Intento 2
+
+    # Intento 2: Si no lo encontró, lo buscamos como String plano (texto)
+    if not documento:
+        documento = db["documents"].find_one({"_id": doc_id})
+        
+    # Subtarea 3: Si probamos de ambas formas y sigue sin estar, devolvemos el 404
+    if not documento:
+        raise HTTPException(status_code=404, detail="No se encontró ningún documento con ese ID.")
+        
+    # Si lo encontró, convertimos el ID a string para que FastAPI lo pueda mostrar en pantalla
+    documento["_id"] = str(documento["_id"])
+    return documento
